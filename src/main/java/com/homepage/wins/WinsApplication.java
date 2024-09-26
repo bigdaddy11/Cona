b89c727f573026f9920cc0c511bc121cd571acc2
Jae Hyun Heo <zidir0070@naver.com>
2024-09-26 15:56:15 +0900

    first commit

---
+++ src/main/java/com/homepage/wins/WinsApplication.java
@@ -0,0 +1,13 @@
+package com.homepage.wins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homepage/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homepage.wins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
